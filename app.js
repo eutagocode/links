5065e493fdf31a8790acab5bfabf107f67912034
eutagocode <thiagoluix21@gmail.com>
2024-11-29 11:32:58 -0300

    feat: :art: Foi feita a importação do ejs e a configuração do mesmo, também foi movido o código de funcionamento para cada pasta para manter o código limpo e organizado

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import linkRoute from "./routes/linkroute.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
@@ -10,27 +15,9 @@ const db = mongoose.connection;
 db.on("error", () => console.log("houve um erro"));
 db.once("open", () => console.log("Banco carregado!"));
 
-const linkSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: String,
-    url: { type: String, required: true },
-    click: { type: Number, default: 0 },
-});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "templates"));
 
-const Link = mongoose.model("Link", linkSchema);
-
-app.get("/:title", async (req, res) => {
-    const title = req.params.title;
-    try {
-        const doc = await Link.findOne({ title });
-        res.redirect(doc.url);
-    } catch (error) {
-        res.send(error);
-    }
-});
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use("/", linkRoute);
 
 app.listen(PORT, () => console.log("Listening on port", PORT));

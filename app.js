7eb01a0fb97ba74bd45087a625b5fe8b74c9124e
eutagocode <thiagoluix21@gmail.com>
2024-12-03 23:21:09 -0300

    feat: :sparkles: A biblioteca cors foi colocada como middleware para todas as rotas

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import cors from "cors";
 import linkRoute from "./routes/linkroute.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
-
+app.use(cors());
 mongoose.connect("mongodb://localhost/newlinks");
 
 const db = mongoose.connection;
